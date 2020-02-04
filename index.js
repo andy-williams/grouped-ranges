1da8de47a42fdd0703203515b2f063f999157b12
andy-williams <williams.andrew.lirio@gmail.com>
2020-02-04 23:12:05 +0000

    wrap as object

--- index.js
+++ index.js
@@ -3,158 +3,170 @@
 // 2. last: = 100
 // 3. start: > 0
 // 4. two items must always have a value 
+function GroupedRanges(rangeEnds) {
+    let _rangeEnds = [...rangeEnds]
 
-// behaviour: 
-// - cannot change from null to a value - use `enableIndexValue`
-// - 
-
-function disableIndex(rangeEnds, indexToDisable) {
-    if (indexToDisable > rangeEnds.length - 1 || indexToDisable < 0) throw 'What are you doing man?'
-
-    const activeRangeEnds =  rangeEnds
-        .filter(x => x != null)
-        .map((x, i) => ({ val: x, idx: i }))
-    const result = [...rangeEnds]
-    const lastActiveEndRange = activeRangeEnds[activeRangeEnds.length - 1]
-    const lastActiveIndex = lastActiveEndRange.idx
-
-    // breaks constraint 4
-    if (activeRangeEnds.length <= 2) return false
-
-    // -- end is an edgecase
-    if (lastActiveIndex == indexToDisable) {
-        result[indexToDisable] = null
-        result[activeRangeEnds[activeRangeEnds.length - 2].idx] = 100 // enforce constraint 2
-        return result
+    function getActiveRangeEnds() {
+        return _rangeEnds
+            .map((x, i) => ({ val: x, idx: i }))
+            .filter(x => x.val != null)
     }
 
-    result[indexToDisable] = null
-    return result
-}
-
-function changeIndexValue(rangeEnds, indexToChange, newValue) {
-    if (indexToChange > rangeEnds.length - 1 || indexToChange < 0) throw 'What are you doing man?'
-
-    const activeRangeEnds =  rangeEnds
-        .filter(x => x != null)
-        .map((x, i) => ({ val: x, idx: i }))
-    const indexToChangeInActiveRange = activeRangeEnds.findIndex(x => x.idx == indexToChange)
-
-    const result = [...rangeEnds]
-    const currentValue = rangeEnds[indexToChange]
-    const isLastIndex = indexToChange == rangeEnds.length - 1
-
-    // optimistic heuristic
-    if (currentValue === newValue) return result 
-
-    // absolutely not allowed
-    if (newValue === null) throw new 'cannot be null'
-    if (currentValue === null) throw 'This isn\'t the place to enable indices'
-
-    if (newValue == 0) return false // breaks constraints 1 and 3 regardless
-    if (newValue > 100) return false // breaks constraint 3
-
-    // incrementing value
-    if(newValue > currentValue) {
-        if (isLastIndex) return false // breaks constraint 3
-        if (newValue >= activeRangeEnds[indexToChangeInActiveRange + 1].val) return false  // -- breaks constraint 1
+    function assertIndexInRange(indexToChange) {
+        if (indexToChange > _rangeEnds.length - 1 || indexToChange < 0) throw 'What are you doing man?'
     }
 
-    // decrementing value
-    if(newValue < currentValue) {
-        if (isLastIndex) return false // breaks constraint 3
-        if (indexToChangeInActiveRange == 0 && activeRangeEnds[indexToChangeInActiveRange + 1].val)
-        if (newValue >= activeRangeEnds[indexToChangeInActiveRange + 1].val) return false  // -- breaks constraint 1
-        if (indexToChange == 0) {
-            result[indexToChange] = newValue;
-            return result
+    this.disableIndex = function disableIndex(indexToDisable) {
+        assertIndexInRange(indexToDisable)
+
+        const activeRangeEnds = getActiveRangeEnds()
+        const lastActiveEndRange = activeRangeEnds[activeRangeEnds.length - 1]
+        const lastActiveIndex = lastActiveEndRange.idx
+
+        // breaks constraint 4
+        if (activeRangeEnds.length <= 2) return false
+
+        // -- end is an edgecase
+        if (lastActiveIndex === indexToDisable) {
+            const secondToLastActiveIndex = activeRangeEnds[activeRangeEnds.length - 2].idx
+            _rangeEnds[indexToDisable] = null
+            _rangeEnds[secondToLastActiveIndex] = 100 // enforce constraint 2
+            return [..._rangeEnds]
         }
+
+        _rangeEnds[indexToDisable] = null
+        return [..._rangeEnds]
     }
 
-    result[indexToChange] = newValue;
-    return result
-}
+    this.changeValue = function changeValue(indexToChange, newValue) {
+        assertIndexInRange(indexToChange)
 
-// constraint:
+        const activeRangeEnds = getActiveRangeEnds()
+        const indexToChangeInActiveRange = activeRangeEnds.findIndex(x => x.idx == indexToChange)
 
-// behaviour:
-// 1. enabling a new item on the right most always sets the previous active most right value to 100
+        const currentValue = rangeEnds[indexToChange]
+        const isLastIndex = indexToChange == rangeEnds.length - 1
 
-function enableIndex(rangeEnds, indexToEnable) {
-    if (indexToEnable > rangeEnds.length - 1 || indexToEnable < 0) throw 'What are you doing man?'
-    if (!isValid(rangeEnds)) throw 'Hey, this is\'t valid yo' // can probably ommit this if we can guarantee validity
+        // optimistic heuristic
+        if (currentValue === newValue) return [...rangeEnds] 
 
-    let result = [...rangeEnds]
-    const activeRangeEnds =  rangeEnds
-        .map((x, i) => ({ val: x, idx: i }))
-        .filter(x => x.val != null || x.idx == indexToEnable)
-    const lastActiveIndex = activeRangeEnds[activeRangeEnds.length - 1].idx
-    const firstActiveIndex = activeRangeEnds[0].idx
-    const indexToChangeInActiveRange = activeRangeEnds.findIndex(x => x.idx == indexToEnable)
+        // absolutely not allowed
+        if (newValue === null) throw new 'cannot be null'
+        if (currentValue === null) throw 'This isn\'t the place to enable indices'
 
-    // why are you enabling an already enabled value?
-    if (rangeEnds[indexToEnable] !== null)  return false
+        if (newValue == 0) return false // breaks constraints 1 and 3 regardless
+        if (newValue > 100) return false // breaks constraint 3
 
-    if (indexToEnable >= lastActiveIndex) {
-        result[indexToEnable] = 100
-        maybeDecrementFromExclusive(result, indexToEnable, 5)
-        return result
+        // incrementing value
+        if(newValue > currentValue) {
+            if (isLastIndex) return false // breaks constraint 3
+            if (newValue >= activeRangeEnds[indexToChangeInActiveRange + 1].val) return false  // -- breaks constraint 1
+        }
+
+        // decrementing value
+        if(newValue < currentValue) {
+            if (isLastIndex) return false // breaks constraint 3
+            if (newValue >= activeRangeEnds[indexToChangeInActiveRange + 1].val) return false  // -- breaks constraint 1
+            if (indexToChange == 0) {
+                rangeEnds[indexToChange] = newValue;
+                return [...rangeEnds]
+            }
+        }
+
+        rangeEnds[indexToChange] = newValue;
+        return [...rangeEnds]
     }
 
-    if (indexToEnable <= firstActiveIndex) {
+    this.enableIndex = function enableIndex(indexToEnable) {
+        assertIndexInRange()
+
+        const activeRangeEnds = getActiveRangeEnds()
+        const lastActiveIndex = activeRangeEnds[activeRangeEnds.length - 1].idx
+        const firstActiveIndex = activeRangeEnds[0].idx
+        const indexToChangeInActiveRange = activeRangeEnds.findIndex(x => x.idx == indexToEnable)
+
+        // why are you enabling an already enabled value?
+        if (_rangeEnds[indexToEnable] !== null)  return false
+
+        // new last active node
+        if (indexToEnable >= lastActiveIndex) {
+            _rangeEnds[indexToEnable] = 100
+            maybeDecrementFromExclusive(indexToEnable, 5)
+            return [..._rangeEnds]
+        }
+
+        // new first active node
+        if (indexToEnable < firstActiveIndex) {
+            const firstActiveValue = _rangeEnds[firstActiveIndex]
+            let newVal = firstActiveValue - 5
+            newVal = newVal < 1 ? 1 : newVal
+            _rangeEnds[indexToEnable] = newVal
+            maybeIncrementFromExclusive(indexToEnable, 5)
+            return [..._rangeEnds]
+        }
+
         const firstActiveValue = rangeEnds[firstActiveIndex]
-        let newVal = firstActiveValue - 5
-        newVal = newVal < 1 ? 1 : newVal
-        result[indexToEnable] = newVal
-        maybeIncrementFromExclusive(result, indexToEnable)
-        return result
+        const prevActiveValue = activeRangeEnds[firstActiveIndex - 1]
+        result[indexToEnable] = activeRangeEnds[indexToChangeInActiveRange + 1].val - 5
+        return [..._rangeEnds]
     }
 
-    const firstActiveValue = rangeEnds[firstActiveIndex]
-    const prevActiveValue = activeRangeEnds[firstActiveIndex - 1]
-    result[indexToEnable] = activeRangeEnds[indexToChangeInActiveRange + 1].val - 5
-    return result
-}
+    this.getValue = function getValue() {
+        return [..._rangeEnds]
+    }
 
-function maybeDecrementFromExclusive(rangeEnds, originatingIndex, firstMinDiff) {
-    const activeRangeEnds =  rangeEnds
-        .map((x, i) => ({ val: x, idx: i }))
-        .filter(x => x.val != null)
-    const activeRangeOriginatingIndex = activeRangeEnds.findIndex(x => x.idx == originatingIndex)
-    const startingIndex = activeRangeOriginatingIndex - 1
+    function maybeDecrementFromExclusive(originatingIndex, firstMinDiff) {
+        const activeRangeEnds =  getActiveRangeEnds()
 
-    for (let i = startingIndex; i > 0; i--) {
-        const currentValue = activeRangeEnds[i].val
-        const rightAdjacentValue = activeRangeEnds[i+1].val
-        const absDiff = Math.abs(currentValue - rightAdjacentValue)
-
-        // edgecase for our special behaviour
-        if(i == startingIndex && absDiff < firstMinDiff) {
-            const newVal = rightAdjacentValue - firstMinDiff
-            activeRangeEnds[i].val = newVal
-            rangeEnds[activeRangeEnds[i].idx] = newVal
-            continue;
-        }
-
-        if (currentValue < rightAdjacentValue) return
-        if (currentValue >= rightAdjacentValue) {
-            activeRangeEnds[i].val = rightAdjacentValue - 1
-            rangeEnds[activeRangeEnds[i].idx] = rightAdjacentValue - 1
+        const activeRangeOriginatingIndex = activeRangeEnds.findIndex(x => x.idx == originatingIndex)
+        const startingIndex = activeRangeOriginatingIndex - 1
+    
+        for (let i = startingIndex; i > 0; i--) {
+            const currentValue = activeRangeEnds[i].val
+            const rightAdjacentValue = activeRangeEnds[i+1].val
+            const absDiff = Math.abs(currentValue - rightAdjacentValue)
+    
+            // edgecase for our special behaviour
+            if (i == startingIndex && absDiff < firstMinDiff) {
+                const newVal = rightAdjacentValue - firstMinDiff
+                activeRangeEnds[i].val = newVal
+                _rangeEnds[activeRangeEnds[i].idx] = newVal
+                continue;
+            }
+    
+            if (currentValue < rightAdjacentValue) return
+            else {
+                activeRangeEnds[i].val = rightAdjacentValue - 1
+                _rangeEnds[activeRangeEnds[i].idx] = rightAdjacentValue - 1
+            }
         }
     }
-}
-
-function maybeIncrementFromExclusive(rangeEnds, fromIndex) {
-    const activeRangeEnds =  rangeEnds
-        .map((x, i) => ({ val: x, idx: i }))
-        .filter(x => x.val != null)
-
-    for (let i =  0; i < activeRangeEnds.length - 2; i++) {
-        if (activeRangeEnds[i+1].val > activeRangeEnds[i].val) return
-        if (activeRangeEnds[i+1].val <= activeRangeEnds[i].val) {
-            const newVal = activeRangeEnds[i].val + 1
-            activeRangeEnds[i+1].val = newVal
-            rangeEnds[activeRangeEnds[i+1].idx] = newVal
+    
+    function maybeIncrementFromExclusive(originatingIndex, firstMinDiff) {
+        const activeRangeEnds =  getActiveRangeEnds()
+    
+        const activeRangeOriginatingIndex = activeRangeEnds.findIndex(x => x.idx == originatingIndex)
+        const startingIndex = activeRangeOriginatingIndex + 1
+    
+        for (let i = startingIndex; i < activeRangeEnds.length; i++) {
+            const currentValue = activeRangeEnds[i].val
+            const leftAdjacentValue = activeRangeEnds[i-1].val
+            const absDiff = Math.abs(currentValue - leftAdjacentValue)
+    
+            // edgecase for our special behaviour
+            if (i == startingIndex && absDiff < firstMinDiff) {
+                const newVal = leftAdjacentValue + firstMinDiff
+                activeRangeEnds[i].val = newVal
+                _rangeEnds[activeRangeEnds[i].idx] = newVal
+                continue;
+            }
+            
+            if (currentValue > leftAdjacentValue) return
+            else {
+                const newVal = leftAdjacentValue + 1
+                activeRangeEnds[i].val = newVal
+                _rangeEnds[activeRangeEnds[i].idx] = newVal
+            }
         }
     }
 }
@@ -182,6 +194,5 @@ function isValid(rangeEnds) {
 }
 
 module.exports = {}
-module.exports.disableIndex = disableIndex
-module.exports.changeIndexValue = changeIndexValue
-module.exports.enableIndex = enableIndex
+module.exports.GroupedRanges = GroupedRanges
+module.exports.GroupedRanges.isValid = isValid
--- index.test.js
+++ index.test.js
@@ -1,120 +1,139 @@
-const { 
-    enableIndex, 
-    disableIndex,  
-    changeIndexValue 
+const {
+    GroupedRanges
 } = require('./index')
 
 // constraints
 // 1. two items must always have a value 
-describe('disableIndex', () => {
-    test('does not allow less than two items to be inactive', () => {
-        expect(disableIndex([1, null, 100], 2)).toBe(false)
-        expect(disableIndex([1, 100], 1)).toBe(false)
-    })
+describe('GroupedRanges', () => {
+    let groupedRanges
 
-    test('disables last active index, and sets new last active index to 100', () => {
-        expect(disableIndex([1, 50, 100], 2)).toStrictEqual([1, 100, null])
-        expect(disableIndex([1, 99, 100], 2)).toStrictEqual([1, 100, null])
-    })
+    function setup(initValue) {
+        groupedRanges = new GroupedRanges(initValue)
+    }
 
-    test('disables item in middle', () => {
-        expect(disableIndex([1, 50, 100], 1)).toStrictEqual([1, null, 100])
-        expect(disableIndex([1, 99, 100], 1)).toStrictEqual([1, null, 100])
-        expect(disableIndex([1, 30, 31, 100], 2)).toStrictEqual([1, 30, null, 100])
-    })
-
-    test('disables first item', () => {
-        expect(disableIndex([1, 50, 100], 0)).toStrictEqual([null, 50, 100])
-        expect(disableIndex([1, 99, 100], 0)).toStrictEqual([null, 99, 100])
-        expect(disableIndex([1, 30, 31, 100], 0)).toStrictEqual([null, 30, 31, 100])
-    })
-})
-
-// constraints
-// 1. all: next > previous
-// 2. last: = 100
-// 3. start: > 0
-describe('changeIndexValue', () => {
-    test('does not allow item to be set to null', () => {
-        expect(() => changeIndexValue([1, 5, 100], 1, null)).toThrow()
-        expect(() => changeIndexValue([1, 5, 100], 2, null)).toThrow()
-    })
-
-    test('does not allow a disabled item to be changed', () => {
-        expect(() => changeIndexValue([1, null, 100], 1, 5)).toThrow()
-        expect(() => changeIndexValue([1, 5, null, 100], 2, 7)).toThrow()
-    })
-
-    test('returns same if value remains the same', () => {
-        expect(changeIndexValue([1, 5, 100], 1, 5)).toStrictEqual([1, 5, 100])
-    })
-
-    describe('incrementing', () => {
-        test('does not allow value to go above 100', () => {
-            expect(changeIndexValue([1, 55, 56, 100], 3, 101)).toBe(false)
-            expect(changeIndexValue([1, 55, 56, 100], 2, 105)).toBe(false)
+    describe('disableIndex', () => {    
+        test('does not allow less than two items to be inactive #1', () => {
+            setup([1, null, 100])
+            expect(groupedRanges.disableIndex(1)).toBe(false)
         })
-
-        test('does not allow the change if increment breaks constraint 1', () => {
-            expect(changeIndexValue([1, 55, 56, 100], 1, 56)).toBe(false)
-            expect(changeIndexValue([1, 55, 56, 100], 1, 57)).toBe(false)
-            expect(changeIndexValue([54, 55, 56, 100], 0, 55)).toBe(false)
-        })
-
-        test('increments the value', () => {
-            expect(changeIndexValue([1, 55, 57, 100], 1, 56)).toStrictEqual([1, 56, 57, 100])
-            expect(changeIndexValue([1, 55, 57, 100], 0, 54)).toStrictEqual([54, 55, 57, 100])
-        })
-    })
-
-    describe('decrementing', () => {
-        test('does not allow first active index to break constraint 3', () => {
-            expect(changeIndexValue([null, 1, 57, 100], 1, 0)).toBe(false)
-            expect(changeIndexValue([1, 2, null, 100], 0, 0)).toBe(false)
-        })
-
-        test('does not allow the change if increment breaks constraint 1', () => {
-            expect(changeIndexValue([1, 55, 57, 100], 2, 55)).toBe(false)
-        })
-
-        test('decrements value', () => {
-            expect(changeIndexValue([1, 55, 56, 100], 1, 54)).toStrictEqual([1, 54, 56, 100])
-            expect(changeIndexValue([6, 55, 56, 100], 0, 4)).toStrictEqual([4, 55, 56, 100])
-        })
-    })
-})
-
-
-describe('enableIndex', () => {
-    test('does not allow enabling an index that\'s already enabled', () => {
-        expect(enableIndex([1, 5, 100], 1)).toBe(false)
-        expect(enableIndex([1, 5, 100], 0)).toBe(false)
-        expect(enableIndex([1, 5, 100], 2)).toBe(false)
-    })
-
-    describe('enabling a new first item', () => {
-        test('increments adjacent items to the right by 1', () => {
-            expect(enableIndex([null, 1, 100], 0)).toStrictEqual([1, 2, 100])
-            expect(enableIndex([null, 1, 2, 3, 100], 0)).toStrictEqual([1, 2, 3, 4, 100])
-            expect(enableIndex([null, 1, 2, 4, 100], 0)).toStrictEqual([1, 2, 3, 4, 100])
-        })
-
-        test('sets value to (right adjacent value)-5', () => {
-            expect(enableIndex([null, 20, 100], 0)).toStrictEqual([15, 20, 100])
-            expect(enableIndex([null, 25, 28, 29, 100], 0)).toStrictEqual([20, 25, 28, 29, 100])
-        })
-    })
-
-    describe('enabling a new last item', () => {
-        expect(enableIndex([1, 100, null], 2)).toStrictEqual([1, 95, 100])
-        expect(enableIndex([1, 98, 99, 100, null], 4)).toStrictEqual([1, 93, 94, 95, 100])
-    })
-
-    describe('enabling middle item', () => {
-        test('sets value to (right adjacent value)-5', () => {
-            expect(enableIndex([15, 20, null, 100], 2)).toStrictEqual([15, 20, 95, 100])
-        })
-    })
-
     
+        test('does not allow less than two items to be inactive #2', () => {
+            setup([1, 100])
+            expect(groupedRanges.disableIndex(1)).toBe(false)
+        })
+    
+        test('disables last item #1', () => {
+            setup([1, 50, 100])
+            expect(groupedRanges.disableIndex(2)).toStrictEqual([1, 100, null])
+        })
+    
+        test('disables last item #2', () => {
+            setup([1, 99, 100])
+            expect(groupedRanges.disableIndex(2)).toStrictEqual([1, 100, null])
+        })
+    
+        test('disables last item #3', () => {
+            setup([1, 50, null, 100])
+            expect(groupedRanges.disableIndex(3)).toStrictEqual([1, 100, null, null])
+        })
+    
+        test('disables item in middle #1', () => {
+            setup([1, 5, 100])
+            expect(groupedRanges.disableIndex(1)).toStrictEqual([1, null, 100])
+        })
+    
+        test('disables item in middle #2', () => {
+            setup([1, 5, 30, null, 100])
+            expect(groupedRanges.disableIndex(2)).toStrictEqual([1, 5, null, null, 100])
+        })
+    
+        test('disables first item #1', () => {
+            setup([1, 50, 100])
+            expect(groupedRanges.disableIndex(0)).toStrictEqual([null, 50, 100])
+        })
+    
+        test('disables first item #2', () => {
+            setup([1, null, 50, 100])
+            expect(groupedRanges.disableIndex(0)).toStrictEqual([null, null, 50, 100])
+        })
+    })
+
+    describe('changeValue', () => {
+        test('returns same if value remains the same', () => {
+            setup([1, 5, 100])
+            expect(groupedRanges.changeValue(1, 5)).toStrictEqual([1, 5, 100])
+        })
+
+        test('does not allow a value to go above 100 #1', () => {
+            setup([1, 55, 56, 100])
+            expect(groupedRanges.changeValue(3, 101)).toBe(false)
+        })
+
+        test('does not allow a value to go above 100 #2', () => {
+            setup([1, 55, 56, 100])
+            expect(groupedRanges.changeValue(2, 105)).toBe(false)
+        })
+
+        test('does not allow the change if increment breaks constraint 1', () => {
+            setup([1, 55, 56, 100])
+            expect(groupedRanges.changeValue(1, 56)).toBe(false)
+        })
+
+        test('increments the value #1', () => {
+            setup([1, 55, 57, 100])
+            expect(groupedRanges.changeValue(1, 56)).toStrictEqual([1, 56, 57, 100])
+        })
+
+        test('increments the value #2', () => {
+            setup([1, 55, null, 57, 100])
+            expect(groupedRanges.changeValue(1, 56)).toStrictEqual([1, 56, null, 57, 100])
+        })
+
+        test('does not allow the change if increment breaks constraint 1 #1', () => {
+            setup([1, 56, null, 57, 100])
+            expect(groupedRanges.changeValue(1, 57)).toBe(false)
+        })
+
+        test('does not allow the change if increment breaks constraint 1 #2', () => {
+            setup([1, 56, 57, 100])
+            expect(groupedRanges.changeValue(1, 57)).toBe(false)
+        })
+
+        test('decrements the value #1', () => {
+            setup([1, 55, 57, 100])
+            expect(groupedRanges.changeValue(2, 56)).toStrictEqual([1, 55, 56, 100])
+        })
+
+        test('decrements the value #2', () => {
+            setup([1, 55, null, 57, 100])
+            expect(groupedRanges.changeValue(3, 56)).toStrictEqual([1, 55, null, 56, 100])
+        })
+    })
+
+    describe('enableIndex', () => {
+        test('enables new active end and decrements adjacent nodes #1', () => {
+            setup([1, 100, null])
+            expect(groupedRanges.enableIndex(2)).toStrictEqual([1, 95, 100])
+        })
+
+        test('enables new active end and decrements adjacent nodes #2', () => {
+            setup([1, 98, 99, 100, null])
+            expect(groupedRanges.enableIndex(4)).toStrictEqual([1, 93, 94, 95, 100])
+        })
+
+        test('enables new active end and decrements adjacent nodes #3', () => {
+            setup([1, 98, null, 99, 100, null])
+            expect(groupedRanges.enableIndex(5)).toStrictEqual([1, 93, null, 94, 95, 100])
+        })
+
+        test('enables new active start #1', () => {
+            setup([null, 6, 100])
+            expect(groupedRanges.enableIndex(0)).toStrictEqual([1, 6, 100])
+        })
+
+        test('enables new active start and increments adjacent nodes #1', () => {
+            setup([null, 1, 100])
+            expect(groupedRanges.enableIndex(0)).toStrictEqual([1, 6, 100])
+        })
+    })
 })
